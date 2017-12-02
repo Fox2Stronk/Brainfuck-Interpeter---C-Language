e646bac26958dd437d6dc72933adf06a256980c7
10gilgil <10gilgil@gmail.com>
2017-12-02 13:55:18 +0200

    reformatted code

--- main.c
+++ main.c
@@ -8,23 +8,19 @@
 
 
 
-int main(int argc, char **argv)
-{   if(argc == 2)
-    {   if(access(argv[1], F_OK)!=-1)
-        {
-            char *content = readFile(argv[1]);
-            interpretBrainFuck(content);
-            return 0;
-        }
-        else
-        {
-            printf("ERROR! Can't open file / file doesn't exist.");
-            return 0;
+int main(int argc, char * * argv) {
+  if (argc == 2) {
+    if (access(argv[1], F_OK) != -1) {
+      char * content = readFile(argv[1]);
+      interpretBrainFuck(content);
+      return 0;
+    } else {
+      printf("ERROR! Can't open file / file doesn't exist.");
+      return 0;
 
-
-        }
     }
-    printf("ERROR! please enter a single argument; the file's path.");
-    return 0;
+  }
+  printf("ERROR! please enter a single argument; the file's path.");
+  return 0;
 }
 
